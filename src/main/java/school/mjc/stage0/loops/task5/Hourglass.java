429655db1c76f1d5837a7d50b490bde7f44065bb
Meerim Sadykbekova <sadykbekova.m@gmail.com>
2023-04-07 21:14:18 +0600

    Completed all tasks.

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -3,5 +3,44 @@ package school.mjc.stage0.loops.task5;
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
 
+        if (height <= 0) {
+            return;
+        }
+        for(int i=0; i < height/2; i++) {
+            for(int j = height - i; j < height; j++) {
+                System.out.print(" ");
+            }
+            for(int j = 0; j < height - 2*i; j++) {
+                System.out.print("8");
+            }
+            for(int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+        if (height % 2 == 0) {
+            for(int i=1; i <= (height+1)/2; i++ )
+            {
+                for(int j = 0; j < height/2 - i; j++)
+                    System.out.print(" ");
+                for(int j = 1; j <= i*2; j++)
+                    System.out.print("8");
+                for(int j = i; j < height/2; j++)
+                    System.out.print(" ");
+                System.out.println();
+            }
+        } else {
+            for(int i=0; i < (height+1)/2; i++ )
+            {
+                for(int j = 1; j <= height/2 - i; j++)
+                    System.out.print(" ");
+                for(int j = 0; j <= i*2; j++)
+                    System.out.print("8");
+                for(int j = i; j < height/2; j++)
+                    System.out.print(" ");
+                System.out.println();
+            }
+        }
     }
 }
